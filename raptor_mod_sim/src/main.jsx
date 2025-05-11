7a4d7b84b9223cf80c34bf3441ddeecca4bb6bab
benice7029 <benice7029@gmail.com>
2025-05-11 17:18:35 +0800

    First edition

---
+++ raptor_mod_sim/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RaptorModSim from "./RaptorModSim";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RaptorModSim />
+  </React.StrictMode>
+);
